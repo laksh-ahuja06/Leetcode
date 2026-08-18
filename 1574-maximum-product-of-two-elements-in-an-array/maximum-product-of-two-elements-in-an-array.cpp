ddfda0792282f8b1f546fbc350ef70c30404e3da
laksh-ahuja06 <ahujalaksh@gmail.com>
2026-08-18 23:20:32 +0530

    Time: 3 ms (15.38%) | Memory: 13.6 MB (20.04%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -7,7 +7,8 @@ public:
         for (int i = 0; i < size - 1; i++) {
             for (int j = i+1; j < size; j++) {
                 int ans = (nums[i] - 1) * (nums[j] - 1);
-                if (ans > finalAns) finalAns = ans;
+                // if (ans > finalAns) finalAns = ans;
+                finalAns = max (finalAns, ans);
             }
         }   
 
