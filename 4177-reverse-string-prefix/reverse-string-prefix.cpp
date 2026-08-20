05990b7898c220cc74fc06a63963bac68cd91a54
laksh-ahuja06 <ahujalaksh@gmail.com>
2026-08-20 12:07:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (9.80%) - LeetSync

--- 4177-reverse-string-prefix/reverse-string-prefix.cpp
+++ 4177-reverse-string-prefix/reverse-string-prefix.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     string reversePrefix(string s, int k) {
-        int length = s.length ();
         string copy = s;
         for (int i = 0; i < k; i++) {
             s[i] = copy[k-1-i];
