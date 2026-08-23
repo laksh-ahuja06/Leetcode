d60f96b7738c7672b181f854eacca880b6f24f80
laksh-ahuja06 <ahujalaksh@gmail.com>
2026-08-23 20:44:06 +0530

    Time: 1621 ms (13.62%) | Memory: 76.6 MB (8.01%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
+        int size = arr.size ();
         vector<int> answer;
 
-        for (int i = 0; i < arr.size()-1; i++) {
+        for (int i = 0; i < size-1; i++) {
             int greatest = 0;
-            for (int j = i+1; j < arr.size(); j++) {
+            for (int j = i+1; j < size; j++) {
                 if (arr[j] > greatest) greatest = arr[j];
             }
 
