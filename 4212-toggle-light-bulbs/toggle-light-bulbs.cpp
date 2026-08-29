6605e456004c1cb0612715e72ee4f6fd0ab711f2
laksh-ahuja06 <ahujalaksh@gmail.com>
2026-08-29 21:47:06 +0530

    Time: 7 ms (23.77%) | Memory: 34.6 MB (44.25%) - LeetSync

--- 4212-toggle-light-bulbs/toggle-light-bulbs.cpp
+++ 4212-toggle-light-bulbs/toggle-light-bulbs.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     vector<int> toggleLightBulbs(vector<int>& bulbs) {
-        vector<int> answer;
         map<int, int> um;
 
         for (int i = 0; i < bulbs.size (); i++) {
             um[bulbs[i]]++;
         }
 
+        bulbs.clear ();
+
         for (auto [key, value] : um) {
             if (value % 2 == 0) continue;
             else {
-                answer.push_back (key);
+                bulbs.push_back (key);
             }
         }
-        return answer;
+        return bulbs;
     }
 };
