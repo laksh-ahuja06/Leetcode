1f33c2f034629a2be0c94f84efc1c1c7d5279cf6
Laksh <ahujalaksh@gmail.com>
2026-09-04 00:38:51 +0530

    Time: 3 ms (52.18%) | Memory: 30.1 MB (87.33%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
 
         // Approach 1: use sorting
         for (int i = 0; i < nums.size (); i++) {
-            nums[i] = nums[i] * nums[i];}
+            nums[i] = pow (abs(nums[i]),2);}
         std::sort (nums.begin (), nums.end ());
         return nums;
 
