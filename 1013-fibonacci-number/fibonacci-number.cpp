c1574c2148151f8d1c553051461930901ba39023
Laksh <ahujalaksh@gmail.com>
2026-09-03 21:24:02 +0530

    Time: 12 ms (32.03%) | Memory: 7.8 MB (38.66%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -3,7 +3,6 @@ public:
     int fib(int n) {
         if (n == 0) return 0;
         if (n == 1) return 1;
-        int answer = fib (n-1) + fib (n-2);
-        return answer;
+        return fib (n-1) + fib (n-2);
     }
 };
