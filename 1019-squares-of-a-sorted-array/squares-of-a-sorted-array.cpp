2f3c5a269c900175387d0dcc3241087039b4ac69
Laksh <ahujalaksh@gmail.com>
2026-09-04 00:37:27 +0530

    Time: 11 ms (16.10%) | Memory: 30.2 MB (66.31%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
+
+        // Approach 1: use sorting
         for (int i = 0; i < nums.size (); i++) {
             nums[i] = pow (abs(nums[i]),2);}
         std::sort (nums.begin (), nums.end ());
         return nums;
+
+        // Approach 2: use 2 pointers
+        // for (int i = 0; i < nums.size (); i++) {
+        //     nums[i] = pow (abs(nums[i]),2);}   
+
+        // int left = 0;
+        // for (int right = 1; right < nums.size (); right++) {
+        //     if (nums[left] > nums[right]) {
+        //         swap (nums[right], nums[left]);
+        //         left++;
+        //     }
+        // }
+
+        // return nums;
     }
 };
